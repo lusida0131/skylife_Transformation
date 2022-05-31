cd667a737d5c8ec00a9c7966f860480a0b6b71fe
lusida0131 <ks12b9189@naver.com>
2022-05-31 17:28:16 +0900

    인텔리제이 연동

---
+++ src/main/java/com/skylife_Transformation/SkylifeTransformationApplication.java
@@ -0,0 +1,13 @@
+package com.skylife_Transformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylifeTransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylifeTransformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skylife_Transformation/SkylifeTransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylife_Transformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylifeTransformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
